ffc11a1d3ea42fae2cfd466306845eae41d31210
melinaco4 <melinaco4@gmail.com>
2022-11-18 16:52:07 +0200

    first commit

---
+++ controllers/company.go
@@ -0,0 +1,86 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/melinaco4/company-manager/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type CompanyController struct {
+	session *mgo.Session
+}
+
+func NewCompanyController(s *mgo.Session) *CompanyController {
+	return &CompanyController{s}
+}
+
+func (uc CompanyController) GetCompany(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.Company{}
+
+	if err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+func (uc CompanyController) CreateCompany(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.Company{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("companies").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc CompanyController) DeleteCompany(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.session.DB("mongo-golang").C("companies").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted company", oid, "\n")
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+
+	"github.com/akhil/mongo-golang/controllers"
+)
+
+func main() {
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/company/:id", uc.GetCompany)
+	r.POST("/company", uc.CreateCompany)
+	r.DELETE("/company/:id", uc.DeleteCompany)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+func getSession() *mgo.Session {
+
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
